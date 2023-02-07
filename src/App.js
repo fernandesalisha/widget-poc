ff995708e58c42807d2ab8033e5ca9b52b4fbbcb
fernandesalisha <110891663+fernandesalisha@users.noreply.github.com>
2023-02-07 11:32:10 +0530

    Reddit Widget improgress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import {Helmet} from "react-helmet";
+
+// Render each post
+function renderPost(post){
+  const { data: { title, url, author, id } } = post
+  const authorUrl = `https://www.reddit.com/u/${author}`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="reddit_widget__post" key={id}>
+      <div className="reddit_widget__posted_by">
+        posted by <a href={authorUrl} className="reddit_widget__posted_by" target="_blank" rel="noopener noreferrer">u/{author}</a>
+      </div>
+      <a href={url} className="reddit_widget__title" target="_blank" rel="noopener noreferrer">{title}</a>
+    </div>
+  )
+}
+
+// Filter, since reddit always returns stickied posts up top
+function nonStickiedOnly(post){
+  return !post.data.stickied
+}
+
+function App({ domElement }) {
+  const subreddit = domElement.getAttribute("data-subreddit")
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState('');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from reddit
+    setLoading(true)
+    fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setData(data.data.children.slice(0, 3));
+      })
+      .catch((e) => {
+        console.log(e)
+        setLoading(false);
+        setError('error fetching from reddit');
+      });
+  }, [ subreddit ])
+
+  return (
+    <div className="reddit_widget__app">
+      <Helmet>
+      <link
+      href="https://giologist.github.io/article-react-reddit-widget/index.css"
+      rel="stylesheet"
+    />
+    <script src="https://giologist.github.io/article-react-reddit-widget/index.js"></script>
+      </Helmet>
+      <h1 className="reddit_widget__header">
+        Latest posts in <a href={`https://reddit.com/r/${subreddit}`} rel="noopener noreferrer">/r/{subreddit}</a>
+      </h1>
+      <div className="reddit_widget__inner">
+        {loading && "Loading..."}
+        {error && error}
+        {!!data.length && data.filter(nonStickiedOnly).map(renderPost)}
+      </div>
+      <p className="reddit_widget__powered_by">
+        This widget is powered by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://javascriptpros.com"
           rel="noopener noreferrer"
+          target="_blank"
         >
-          Learn React
+          JavaScriptPros.com
         </a>
-      </header>
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+// Find all widget divs
+const WidgetDivs = document.querySelectorAll('.widget')
+// Inject our React App into each
+WidgetDivs.forEach(Div => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={Div} />
+    </React.StrictMode>,
+    Div
+  );
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
